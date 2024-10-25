2d40db156fb23a4270e8f67e5bff8f66f2a601b9
jay <jay569905@gmail.com>
2024-10-26 01:50:32 +0200

    adds Task 15

---
+++ src/main/java/lesson7/Task15RevertArray.java
@@ -0,0 +1,30 @@
+package lesson7;
+
+// Создать массив, заполнить его случайными элементами, распечатать,
+//перевернуть, и снова распечатать (при переворачивании нежелательно создавать
+//еще один массив).
+
+public class Task15RevertArray {
+    public static void main(String[] args) {
+        int[] lol = {1, 2, 3, 4, 5};
+        int temp = 0;
+
+        for (int i : lol) {
+            System.out.print(i);
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < lol.length; i++) {
+            for (int j = i; j < lol.length; j++) {
+                temp = lol[j];
+                lol[j] = lol[lol.length - 1];
+                lol[lol.length - 1] = temp;
+            }
+        }
+
+        for (int i : lol) {
+            System.out.print(i);
+        }
+    }
+}
